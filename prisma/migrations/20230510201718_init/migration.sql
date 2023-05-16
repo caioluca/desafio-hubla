b318611fa7632a1cb5fffc6614a3c1fe825062f4
Caio Luca <caioluca.job@gmail.com>
2023-05-16 07:19:15 -0300

    build(database): clear database and migrations

--- prisma/migrations/20230510201718_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Transaction" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "type" TEXT NOT NULL,
-    "date" DATETIME NOT NULL,
-    "product" TEXT NOT NULL,
-    "value" TEXT NOT NULL,
-    "seller" TEXT NOT NULL
-);
